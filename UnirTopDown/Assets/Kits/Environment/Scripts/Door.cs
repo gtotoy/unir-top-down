9be15616d4e9c8caf91511e8edfcbc37080fc2a1
Luis Jesus Diaz Arboleda <luisdiazarboleda97@gmail.com>
2026-02-20 13:59:19 -0500

    Add open door animation

--- UnirTopDown/Assets/Kits/Environment/Scripts/Door.cs
+++ UnirTopDown/Assets/Kits/Environment/Scripts/Door.cs
@@ -5,18 +5,33 @@ public class Door : MonoBehaviour
     [SerializeField] GameObject canvasMessage;
     [SerializeField] ZoneController zoneController;
     private bool isLocked = true;
+    private Animator anim;
 
     private void Awake()
     {
         canvasMessage.SetActive(false);
+        anim = GetComponent<Animator>();
+        anim.SetBool("isLocked", isLocked);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        canvasMessage.SetActive(true);
+        if (isLocked)
+        {
+            canvasMessage.SetActive(true);
+        }
+        else
+        {
+            canvasMessage.SetActive(false);
+            anim.SetBool("isLocked", isLocked);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        canvasMessage.SetActive(false);
+        if (isLocked)
+        { 
+            canvasMessage.SetActive(false);
+        }
     }
 
     public void setIsUnlocked()
